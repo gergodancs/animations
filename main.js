fd37721b9e7fa4506a4fc2b2927177c868e5c38e
hunbp75 <hunbp75@gmail.com>
2021-12-02 14:03:34 +0100

    :tada:

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-function newPage() {
-    setTimeout(function() {
-        window.location.href = 'landing.html';
-    }, 10500);
+function hide() {
+    document.getElementById('animation').style.display = 'none'; 
+    document.getElementById('vonal').style.display = 'none'; 
+    document.getElementById('feri').style.display = 'none'; 
+    document.getElementById('loader').style.display = 'none'; 
+    document.getElementById('andi').style.display = 'none'; 
+    document.getElementById('loader2').style.display = 'none'; 
+    document.getElementById('content').style.visibility = 'visible'; 
 }
-
-window.addEventListener("load", newPage);
+ 
+window.addEventListener ("load", function() {
+    setTimeout(hide, 10500);
+})
